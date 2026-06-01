fa56f1a712b8fd25190ee7fbb46780e0e9a05697
Xavier Garcia <00014624@uca.edu.sv>
2026-06-01 17:34:39 -0600

    Pantallas
    Arreglando pantallas

--- app/src/main/java/com/pdmcourse2026/basictemplate/screens/home/HomeScreen.kt
+++ app/src/main/java/com/pdmcourse2026/basictemplate/screens/home/HomeScreen.kt
@@ -1,7 +1,9 @@
 package com.pdmcourse2026.basictemplate.screens.home
 
 import android.service.autofill.OnClickAction
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -11,7 +13,9 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -27,8 +31,21 @@ fun HomeScreen() {
       )
     }
   ) { innerPadding ->
-    Column(modifier = Modifier.padding(innerPadding)) {
-      Text(text = "Home Screen")
+    Column(modifier = Modifier
+      .padding(innerPadding)
+      .fillMaxSize(),
+      horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+      Text(
+        text = "Seleciona por cual quieres votar",
+        modifier = Modifier.padding(bottom = 16.dp)
+      )
+      
+      Button(onClick = {
+      }) {
+        Text(text = "Ir a Resultados ")
+      }
+
     }
 
   }
--- app/src/main/java/com/pdmcourse2026/basictemplate/screens/resultados/ResultadosScreen.kt
+++ app/src/main/java/com/pdmcourse2026/basictemplate/screens/resultados/ResultadosScreen.kt
@@ -1,7 +1,10 @@
 package com.pdmcourse2026.basictemplate.screens.resultados
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -9,7 +12,10 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigationevent.compose.rememberNavigationEventState
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -25,9 +31,22 @@ fun ResultadosScreen() {
             )
         }
     ) { innerPadding ->
-        Column(modifier = Modifier.padding(innerPadding)) {
-            Text(text = "Resultados Screen")
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "Resultados Screen",
+                modifier = Modifier.padding(bottom = 16.dp)
+            )
+
+            Button(onClick = {
+            }) {
+                Text(text = "Nuevo Volver a votar")
+            }
         }
     }
-
 }
